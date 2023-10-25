431a4020509a3623080566313a59116116ef2d1f
Abhishek <abhipatil2630@gmail.com>
2023-10-25 14:01:08 +0530

    Another_Activity are added in the project

--- app/src/main/java/com/example/wscube/AnotherActivity.java
+++ app/src/main/java/com/example/wscube/AnotherActivity.java
@@ -1,11 +1,15 @@
 package com.example.wscube;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
 public class AnotherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_another);
+        String st = "Viraj benade";
     }
 }
